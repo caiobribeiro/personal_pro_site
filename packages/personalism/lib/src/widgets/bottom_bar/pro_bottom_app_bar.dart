e943d31cd38ecb3de7127d4c223f1580dec1953d
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2023-02-14 09:23:02 -0300

    A custom navigation bar created on package Personalism

---
+++ packages/personalism/lib/src/widgets/bottom_bar/pro_bottom_app_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:personalism/src/effects/glass_box_effect.dart';
+
+class ProBottomBar extends StatefulWidget {
+  const ProBottomBar({super.key});
+
+  @override
+  State<ProBottomBar> createState() => _ProBottomBarState();
+}
+
+class _ProBottomBarState extends State<ProBottomBar> {
+  int _selected = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: FrostedGlassBox(
+        theHeight: 90.0,
+        theWidth: 70.0,
+        theChild: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconSize: 50,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black38,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.description),
+              label: 'Curriculum',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.terminal),
+              label: 'Projects',
+            ),
+          ],
+          currentIndex: _selected,
+          onTap: (index) {
+            setState(() {
+              _selected = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
